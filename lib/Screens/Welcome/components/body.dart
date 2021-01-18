09b347fe84e264983504a7a9f8e9dc8b94ae92d3
sawthinkar <10482589+sawthinkar@users.noreply.github.com>
2021-01-19 02:08:57 +0630

    add LogIn button, and Welcome screen

--- lib/Screens/Welcome/components/body.dart
+++ lib/Screens/Welcome/components/body.dart
@@ -11,10 +11,27 @@ class Body extends StatelessWidget {
         child: Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: <Widget>[
-        Text("Welcome to Twitter"),
+        Text(
+          "Welcome to Twitter",
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
         SvgPicture.asset(
           "assets/icons/signup.svg",
-          height: size.height * 0.45,
+          height: size.height * 0.35,
+        ),
+        Container(
+          width: size.width * 0.8,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: FlatButton(
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+                color: kPrimaryLightColor,
+                onPressed: () {},
+                child: Text(
+                  "Log In",
+                  style: TextStyle(color: kPrimaryColor),
+                )),
+          ),
         )
       ],
     ));
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xFF6F35B2);
-const kPrimayLightColor = Color(0xF5F8FAA5);
+// Brand Palettes <https://brandpalettes.com/twitter-colors/>
+// const kPrimaryColor = Color(0xFF6F35B2);
+const kPrimaryColor = Color.fromARGB(255, 29, 161, 242);
+// const kPrimayLightColor = Color(0xF5F8FAA5);
+const kPrimaryLightColor = Color.fromARGB(255, 245, 248, 250);
+const kPrimaryDarkColor = Color.fromARGB(255, 20, 23, 26);
