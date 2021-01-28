0e6a38e064fd2bc3c6fbf6a1f3cd1399da733d30
sawthinkar <10482589+sawthinkar@users.noreply.github.com>
2021-01-28 08:46:47 +0630

    add TextFieldContainer and RoundedCornerInputField

--- lib/Screens/Login/components/body.dart
+++ lib/Screens/Login/components/body.dart
@@ -38,6 +38,20 @@ class Body extends StatelessWidget {
             ),
           ),
           SizedBox(height: size.height * 0.03),
+
+          // Widget Extracted Method
+          RoundedCornerInputField(
+            hintText: "Phone, email, or username",
+            onChanged: (value) {},
+            icon: Icons.person,
+          ),
+          RoundedCornerInputField(
+            hintText: "Password",
+            onChanged: (value) {},
+            icon: Icons.lock,
+          ),
+
+          // Simple TextFormField Method
           Padding(
             padding: const EdgeInsets.all(10.0),
             child: TextFormField(
@@ -54,6 +68,8 @@ class Body extends StatelessWidget {
               ),
             ),
           ),
+
+          // Simple
           RoundedButton(
             text: "Log in",
             textColor: kSecondaryExExLightColor,
@@ -162,6 +178,59 @@ class Body extends StatelessWidget {
           // ),
         ],
       ),
+    );
+  }
+}
+
+class RoundedCornerInputField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedCornerInputField({
+    Key key,
+    this.hintText,
+    this.icon,
+    // this.icon = Icons.person,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
+
+class TextFieldContainer extends StatelessWidget {
+  final Widget child;
+  const TextFieldContainer({
+    Key key,
+    this.child,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+      width: size.width * 0.8,
+      decoration: BoxDecoration(
+        color: kSecondaryExExLightColor,
+        borderRadius: BorderRadius.circular(5),
+      ),
+      child: child,
     );
   }
 }
